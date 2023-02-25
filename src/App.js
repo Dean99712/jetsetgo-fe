Dean99712/FlightBooking-FrontEnd

improving offerCard logic & add custom Card component

src/App.js
@@ -2,30 +2,35 @@ import './styles/App.scss';
 import * as React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import BookFlightComponent from "./components/BookFlightComponent";
+import BookFlightComponent from "./components/booking/BookFlightComponent";
 import Header from "./components/Header";
-import OffersComponent from "./components/OffersComponent";
 import 'react-date-range/dist/styles.css'
 import 'react-date-range/dist/theme/default.css';
 import OrdersComponent from "./components/OrdersComponent";
-import Skeleton, {SkeletonTheme} from "react-loading-skeleton";
-
+import {SkeletonTheme} from "react-loading-skeleton";
+import PassengersDetailsUpdateComponent from "./components/PassengersDetailsUpdateComponent";
+import "react-loading-skeleton/dist/skeleton.css";
+import FlightsComponent from "./components/FlightsComponent";
+import './styles/FlightsOfferComponent.scss'
+import FareOptions from "./components/FareOptions";
+import Login from "./components/login/Login";
 
 function App() {
 
     return (
         <div>
-            <SkeletonTheme baseColor="#202020" highlightColor="#444">
-            <Router>
-                <Header/>
-                <div className="container">
+            <SkeletonTheme baseColor={"#b4b4b4"} highlightColor={"#e8e8e8"}>
+                <Router>
+                    <Header/>
                     <Routes>
                         <Route path="/" element={<BookFlightComponent/>}></Route>
                         <Route path="/orders" element={<OrdersComponent/>}></Route>
-                        <Route path="/offers" element={<OffersComponent/>}></Route>
+                        <Route path="booking/flights" element={<FlightsComponent/>}></Route>
+                        <Route path="/fare_options" element={<FareOptions/>}></Route>
+                        <Route path="/passengers" element={<PassengersDetailsUpdateComponent/>}></Route>
+                        <Route path="/login" element={<Login/>}></Route>-
                     </Routes>
-                </div>
-            </Router>
+                </Router>
             </SkeletonTheme>
         </div>
     );

Dean99712/jetsetgo-fe

src/App.js
@@ -2,30 +2,37 @@ import './styles/App.scss';
 import * as React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import BookFlightComponent from "./components/BookFlightComponent";
+import BookFlight from "./components/booking/BookFlight";
 import Header from "./components/Header";
-import OffersComponent from "./components/OffersComponent";
 import 'react-date-range/dist/styles.css'
 import 'react-date-range/dist/theme/default.css';
 import OrdersComponent from "./components/OrdersComponent";
-import Skeleton, {SkeletonTheme} from "react-loading-skeleton";
-
+import {SkeletonTheme} from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import FlightsComponent from "./components/FlightsComponent";
+import './styles/FlightsOfferComponent.scss'
+import FareOptionsComponent from "./components/fareoptions/FareOptionsComponent";
+import Login from "./components/login/Login";
+import Footer from "./components/Footer";
+import CreateOrderComponent from "./components/ordercreate/CreateOrderComponent";
 
 function App() {
 
     return (
         <div>
-            <SkeletonTheme baseColor="#202020" highlightColor="#444">
-            <Router>
-                <Header/>
-                <div className="container">
+            <SkeletonTheme baseColor={"#b4b4b4"} highlightColor={"#e8e8e8"}>
+                <Router>
+                    <Header/>
                     <Routes>
-                        <Route path="/" element={<BookFlightComponent/>}></Route>
+                        <Route path="/" element={<BookFlight/>}></Route>
                         <Route path="/orders" element={<OrdersComponent/>}></Route>
-                        <Route path="/offers" element={<OffersComponent/>}></Route>
+                        <Route path="booking/flights" element={<FlightsComponent/>}></Route>
+                        <Route path="/fare_options" element={<FareOptionsComponent/>}></Route>
+                        <Route path="/flight_checkout" element={<CreateOrderComponent/>}></Route>
+                        <Route path="/login" element={<Login/>}></Route>
                     </Routes>
-                </div>
-            </Router>
+                    <Footer/>\
+                </Router>
             </SkeletonTheme>
         </div>
     );