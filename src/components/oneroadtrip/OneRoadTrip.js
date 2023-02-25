0bb7d7261afd3dda7d0957b553d9ae285e014b9a
Dean99712 <dean2910997@gmail.com>
2023-02-26 00:09:26 +0200

    improving offerCard logic & add custom Card component

--- src/components/oneroadtrip/OneRoadTrip.js
+++ src/components/oneroadtrip/OneRoadTrip.js
@@ -1,52 +1,51 @@
-import React from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faRepeat} from "@fortawesome/free-solid-svg-icons";
+import {faPlaneArrival, faPlaneDeparture} from "@fortawesome/free-solid-svg-icons";
+import DatePicker from "../datepicker/DatePickerComponent";
 
 const OneRoadTrip = props => {
 
-    return (
-        <div style={{display:"flex", gap:'1em'}}>
-                <div className="search-location">
-                    {/* Origin*/}
-                    <div className="search-input" id="origin-container">
-                        <input
-                            type="text"
-                            name="slices.origin"
-                            placeholder="From"
-                            maxLength={3}
-                            autoComplete="off"
-                            ref={props.origin}
-                        />
-                        <label>Where From?</label>
-                    </div>
+    function onChange(timestamp) {
 
-                    {/* Destination*/}
-                    <FontAwesomeIcon className="switch-locations" icon={faRepeat}/>
-                    <div className="search-input" id="destination-container">
-                        <input
-                            type="text"
-                            name="slices.destination"
-                            placeholder="To"
-                            maxLength={3}
-                            autoComplete="off"
-                            ref={props.destination}
-                        />
+    }
+
+    return (
+        <div style={{display: "flex", gap: '1em'}}>
+            <div className="search-location">
+                {/* Origin*/}
+                <div className="search-input" id="origin-container">
+                    <div><FontAwesomeIcon className="icon" icon={faPlaneDeparture}/></div>
+                    <div><input
+                        type="text"
+                        name="slices.origin"
+                        placeholder="From"
+                        maxLength={3}
+                        autoComplete="off"
+                        ref={props.origin}
+                    />
+                        <label>Where From?</label></div>
+                </div>
+
+                {/* Destination*/}
+                <div className="search-input" id="destination-container">
+                    <div><FontAwesomeIcon className="icon" icon={faPlaneArrival}/></div>
+                    <div><input
+                        type="text"
+                        name="slices.destination"
+                        placeholder="To"
+                        maxLength={3}
+                        autoComplete="off"
+                        ref={props.destination}
+                    />
                         <label>Where to?</label>
                     </div>
-                    {/*Departure Date*/}
                 </div>
-                <div className="search-input search-date" id="departure_date-container"
-                    // onClick={() => setOpenDepartureDate(true)}
-                >
-                    {/*{openDepartureDate && <DateRangeComponent departureDate={departure_date}/>}*/}
-                    <input
-                        type="date"
-                        name="slices.departure_date"
-                        placeholder="Departure Date"
-                        ref={props.departureDate}
-                    />
-                    <label>Departure Date</label>
-                </div>
+                {/*Departure Date*/}
+            </div>
+            <div className="search-input search-date">
+                <label>Departure Date</label>
+                <DatePicker onChange={onChange} departureDate={props.departureDate} />
+
+            </div>
         </div>
     );
 };
